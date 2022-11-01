f9b81d933b2be36b200b4e3bb6f45bb194123d78
kadenyi <treykadenyi@gmail.com>
2022-11-01 23:38:51 +0300

    Create FunCorner folder

---
+++ src/Components/Meals/FunCorner/FunCorner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Navbar from "../Navbar/Navbar";
+
+const FunCorner = () => {
+  return (
+    <section>
+      <Navbar type="fun_corner" />
+    </section>
+  );
+};
+
+export default FunCorner;
