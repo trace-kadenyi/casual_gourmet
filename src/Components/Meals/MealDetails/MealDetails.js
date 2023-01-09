acf71a4d1772b98b462136c538ceba8157b62dc5
kadenyi <treykadenyi@gmail.com>
2023-01-09 15:34:13 +0300

    Remove unnecessary import

--- src/Components/Meals/MealDetails/MealDetails.js
+++ src/Components/Meals/MealDetails/MealDetails.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
 import axios from "axios";
 
 // import "bulma/css/bulma.min.css";
