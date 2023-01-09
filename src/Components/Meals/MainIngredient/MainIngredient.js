f419e2fd6758c745fec6c047d0ef6d94f894da19
kadenyi <treykadenyi@gmail.com>
2023-01-09 19:28:16 +0300

    Create component for main ingredients

---
+++ src/Components/Meals/MainIngredient/MainIngredient.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+
+import Navbar from "../MealsNavigation/Navbar";
+
+
+const MainIngredient = () => {
+  const [ingredient, setIngredient] = useState("");
+  const [fetchedMainIngredient, setFetchedMainIngredient] = useState([]);
+  const navigate = useNavigate();
+
+  // fetch meals based on main ingredients
+  let mainIngredientsContainer;
+
+  const fetchByMainIngredient = async () => {
+    await axios
+      .get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+      .then((response) => {
+        mainIngredientsContainer = response.data.meals;
+        setFetchedMainIngredient(mainIngredientsContainer);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // console.log(fetchedMainIngredient);
+
+  const handleIndividualMeal = (id) => {
+    navigate(`/meals_categories/:category/${id}`);
+  };
+
+  return (
+    <section>
+       <Navbar type="meal_details" />
+      <div>
+        <h1>Search by Main Ingredient</h1>
+        <input
+          type="text"
+          placeholder="Enter main ingredient"
+          value={ingredient}
+          onChange={(e) => setIngredient(e.target.value)}
+        />
+        <button onClick={fetchByMainIngredient}>Go</button>
+
+        {fetchedMainIngredient.map((item) => {
+          return (
+            <div key={item.idMeal}>
+              <h3>{item.strMeal}</h3>
+              <img
+                className="recipe_image"
+                src={item.strMealThumb}
+                alt={item.strMeal}
+                onClick={() => handleIndividualMeal(item.idMeal)}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default MainIngredient;
