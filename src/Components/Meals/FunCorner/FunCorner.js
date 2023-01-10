32dffb4530da779411df86217b092e199188c41a
kadenyi <treykadenyi@gmail.com>
2023-01-10 12:19:38 +0300

    Add buttons for the first letter and name searches

--- src/Components/Meals/FunCorner/FunCorner.js
+++ src/Components/Meals/FunCorner/FunCorner.js
@@ -1,84 +1,110 @@
-import React, {useEffect, useState} from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import swal from 'sweetalert';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 
-
 import Navbar from "../MealsNavigation/Navbar";
-import MealsPerArea from "../Areas/MealsPerArea";
 import { fetchAreas } from "../../../Redux/Meals/areaslice";
 
 import "./fun_corner.css";
 
 const FunCorner = () => {
   const [country, setCountry] = useState("");
-  // const [ingredient, setIngredient] = useState("");
   const navigate = useNavigate();
   const { areas, loading } = useSelector((state) => state.areas);
   const dispatch = useDispatch();
-    const [fetchedMainIngredient, setFetchedMainIngredient] = useState([]);
 
-  
+  // fetch areas on load
   useEffect(() => {
     dispatch(fetchAreas());
   }, []);
 
-  // const { area } = useParams();
-
+  // handle country
   const foundArea = areas.find((area) => area.strArea === country);
-  
 
   const handleCountry = () => {
     if (foundArea) {
       navigate(`/areas/${country}`);
-    }
-    else {
+    } else {
       swal(
         "NB: Some countries are not represented in the database.",
-        "Please enter a valid country of origin and capitalize the first letter (i.e. American, British, Canadian, Chinese, Croatian, Dutch, Egyptian, French, Greek, Indian, Irish, Italian, Jamaican, Japanese, Kenyan, Malaysian, Mexican, Moroccan, Polish, Portuguese, Russian, Spanish, Thai, Tunisian, Turkish, Unknown, Vietnamese).")
+        "Please enter a valid country of origin and capitalize the first letter (i.e. American, British, Canadian, Chinese, Croatian, Dutch, Egyptian, French, Greek, Indian, Irish, Italian, Jamaican, Japanese, Kenyan, Malaysian, Mexican, Moroccan, Polish, Portuguese, Russian, Spanish, Thai, Tunisian, Turkish, Unknown, Vietnamese)."
+      );
     }
-    
-  }
+  };
 
+  // handle main ingredient
   const handleMainIngredient = () => {
     navigate(`/main_ingredient`);
-  }
+  };
 
+  // handle first letter
+  const handleFirstLetter = () => {
+    navigate("/first_letter");
+  };
 
- 
+  // handle name
+  const handleName = () => {
+    navigate("/name");
+  };
 
   return (
     <section className="fun_corner_sect">
       <Navbar type="fun_corner" />
       <div className="fun_corner_div">
         <h3>Fun Corner</h3>
-        <p>
-          Spice up your cooking experience in this little fun corner. </p>
+        <p>Spice up your cooking experience in this little fun corner. </p>
         <div className="searches">
           {/* country of origin */}
           <div>
-        <input type="text" placeholder="Search by Country of Origin" className="fun_input" value={country} onChange={(e) => setCountry(e.target.value)} />
-            <button className="go" onClick={handleCountry}>Go</button>
+            <input
+              type="text"
+              placeholder="Search by Country of Origin"
+              className="fun_input"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+            />
+            <button className="go" onClick={handleCountry}>
+              Go
+            </button>
           </div>
 
-        {/* {/* main ingredient */}
-       
-        <button className="main_ingredient_btn" onClick={handleMainIngredient}>Search By Main Ingredient</button>
-          
+          {/* {/* main ingredient */}
+          <div>
+            <button
+              className="main_ingredient_btn"
+              onClick={handleMainIngredient}
+            >
+              Search By Main Ingredient
+            </button>
           </div>
-          {/* <ul>
-            <li>Country of Origin</li>
-            <li>First letter</li>
-            <li>Main ingredient</li>
+          {/* first letter */}
+          <div>
+            <button className="first_letter_btn" onClick={handleFirstLetter}>
+              Search By First Letter
+            </button>
+          </div>
+
+          {/* name */}
+          <div>
+            <button className="name_btn" onClick={handleName}>
+              Search By Name
+            </button>
+          </div>
+        </div>
+        {/* <ul>
+         
             <li>Name</li>
           </ul> */}
 
-        <p>Want to be even more spontaneous? Click the random button to get a random meal.</p>
-
+        <p>
+          Want to be even more spontaneous? Click the random button to get a
+          random meal.
+        </p>
       </div>
 
-       {/* <div className="todo">
+      {/* <div className="todo">
         <h3>TODO</h3>
         <p>
         // spice up your cooking experience in this little fun corner. You can search for meals by:
