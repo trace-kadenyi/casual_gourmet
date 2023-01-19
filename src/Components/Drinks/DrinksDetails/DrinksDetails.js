791e24049c08d9a43424e01be2a27f1fd1343370
kadenyi <treykadenyi@gmail.com>
2023-01-19 13:00:47 +0300

    Fetch and display ingredients as well as add back button

--- src/Components/Drinks/DrinksDetails/DrinksDetails.js
+++ src/Components/Drinks/DrinksDetails/DrinksDetails.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import "./drinks_details.css";
 
@@ -9,6 +9,7 @@ const DrinksDetails = () => {
   const { categories, loading, fulfilled } = useSelector(
     (state) => state.drinksCategories
   );
+  const navigate = useNavigate();
 
   const [fetchedDrinks, setFetchedDrinks] = useState([]);
   const { id } = useParams();
@@ -30,6 +31,26 @@ const DrinksDetails = () => {
       });
   };
 
+  // fetch ingredients and measures from API
+
+  const ingredients = [];
+
+  const handleIngredients = () => {
+    if (fetchedDrinks) {
+      for (let i = 1; i <= 20; i++) {
+        if (fetchedDrinks[0][`strIngredient${i}`]) {
+          ingredients.push(
+            `${fetchedDrinks[0][`strIngredient${i}`]} - ${
+              fetchedDrinks[0][`strMeasure${i}`]
+            }`
+          );
+        }
+      }
+      return ingredients;
+    }
+  };
+
+  // fetch drinks details on page load
   useEffect(() => {
     fetchDrinksDetails();
   }, []);
@@ -52,6 +73,14 @@ const DrinksDetails = () => {
                   />
                   <p>{drink.strTags}</p>
                   <p>{drink.strCategory}</p>
+                  <button
+                    className="back_btn"
+                    onClick={() => {
+                      navigate(`/drinks_categories/${drink.strCategory}`);
+                    }}
+                  >
+                    Back to {drink.strCategory}
+                  </button>
                   <p>{drink.strIBA}</p>
                   <p>{drink.strAlcoholic}</p>
                   <p>{drink.strGlass}</p>
@@ -60,6 +89,29 @@ const DrinksDetails = () => {
                   <p>{drink.strInstructionsDE}</p>
                   <p>{drink.strInstructionsFR}</p>
                   <p>{drink.strInstructionsIT}</p>
+                  {/* display ingredients */}
+                  <div className="ingredients_div">
+                    <h2 className="ingredients_heading">Ingredients</h2>
+                    <div className="ingredients_ul">
+                      {handleIngredients().map((ingredient, index) => (
+                        <div key={index} className="column is-half">
+                          <div className="card ingredient_card">
+                            <div className="card-content">
+                              <div className="content">
+                                <p className="ingredient_span">
+                                  <span>{ingredient.split("-")[0]} </span>
+                                  <span>-</span>
+                                  <span className="measurement">
+                                    {ingredient.split("-")[1]}
+                                  </span>
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
                 </div>
               );
             })
