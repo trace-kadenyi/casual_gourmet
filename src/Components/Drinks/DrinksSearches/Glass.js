9c9aa800c66f7e194812be21d4692de7b90a1a93
kadenyi <treykadenyi@gmail.com>
2023-01-20 13:20:52 +0300

    Fetch and display data

--- src/Components/Drinks/DrinksSearches/Glass.js
+++ src/Components/Drinks/DrinksSearches/Glass.js
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import DrinksNavigation from "../DrinksNavigation/DrinksNavigation";
 
 const Glass = () => {
-  return (
-    <div>Glass</div>
-  )
-}
+  const [glass, setGlass] = useState("");
+  const [fetchedGlass, setFetchedGlass] = useState([]);
+  const navigate = useNavigate();
+  const { categories, loading, fulfilled, rejected } = useSelector(
+    (state) => state.drinksCategories
+  );
 
-export default Glass
+  const { category } = useParams();
+
+  // fetch drinks by glass
+  const BASE_URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glass}`;
+
+  let glassContainer;
+
+  const fetchGlass = async () => {
+    await axios
+      .get(BASE_URL)
+      .then((response) => {
+        glassContainer = response.data.drinks;
+        setFetchedGlass(glassContainer);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // handle individual drinks
+  const handleIndividualDrinks = (id) => {
+    navigate(`/drinks_categories/:category/${id}`);
+    console.log(category);
+  };
+
+  return (
+    <section>
+      <DrinksNavigation type="category" />
+      <div className="search_div">
+        <h1 className="search_heading">Search By Glass</h1>
+        <div className="input_go">
+          <input
+            className="search_input"
+            type="text"
+            placeholder="Enter glass"
+            autoFocus
+            value={glass}
+            onChange={(e) => setGlass(e.target.value)}
+          />
+          <button className="go_btn" onClick={fetchGlass}>
+            Go
+          </button>
+        </div>
+        {/* display drinks if present in the database */}
+        <div className="one_cat">
+          {fetchedGlass ? (
+            fetchedGlass.map((item) => {
+              return (
+                <div className="individual_items" key={item.idDrink}>
+                  <img
+                    className="recipe_image"
+                    src={item.strDrinkThumb}
+                    alt={item.strDrink}
+                    onClick={() => handleIndividualDrinks(item.idDrink)}
+                  />
+                  <h3 className="meal_title">{item.strDrink}</h3>
+                </div>
+              );
+            })
+          ) : (
+            <h1 className="no_recipe">No recipe found</h1>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Glass;
--- src/Components/Drinks/DrinksSearches/Name.js
+++ src/Components/Drinks/DrinksSearches/Name.js
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import DrinksNavigation from "../DrinksNavigation/DrinksNavigation";
 
 const Name = () => {
-  return (
-    <div>Name</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [fetchedName, setFetchedName] = useState([]);
+  const navigate = useNavigate();
+  const { categories, loading, fulfilled, rejected } = useSelector(
+    (state) => state.drinksCategories
+  );
 
-export default Name
+  const { category } = useParams();
+
+  // fetch drinks by name
+  const BASE_URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
+
+  let nameContainer;
+
+  const fetchName = async () => {
+    await axios
+      .get(BASE_URL)
+      .then((response) => {
+        nameContainer = response.data.drinks;
+        setFetchedName(nameContainer);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // handle individual drinks
+  const handleIndividualDrinks = (id) => {
+    navigate(`/drinks_categories/:category/${id}`);
+    console.log(category);
+  };
+
+  return (
+    <section>
+      <DrinksNavigation type="category" />
+      <div className="search_div">
+        <h1 className="search_heading">Search By Name</h1>
+        <div className="input_go">
+          <input
+            className="search_input"
+            type="text"
+            placeholder="Enter name"
+            autoFocus
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button className="go_btn" onClick={fetchName}>
+            Go
+          </button>
+        </div>
+        {/* display drinks if present in the database */}
+        <div className="one_cat">
+          {fetchedName ? (
+            fetchedName.map((item) => {
+              return (
+                <div className="individual_items" key={item.idDrink}>
+                  <img
+                    className="recipe_image"
+                    src={item.strDrinkThumb}
+                    alt={item.strDrink}
+                    onClick={() => handleIndividualDrinks(item.idDrink)}
+                  />
+                  <h3 className="meal_title">{item.strDrink}</h3>
+                </div>
+              );
+            })
+          ) : (
+            <h1 className="no_recipe">No recipe found</h1>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Name;
